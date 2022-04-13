86f7240d74ac15dfc5edf4cd9aede9304ef54237
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-04-12 22:19:52 -0400

    Update app UI

---
+++ styles.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+footer="""<style>
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+color: white;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>AI - COMP 377 | Group 6 <a style='display: block; text-align: center;' href="https://github.com/mengxi-li/group-project" target="_blank">Github</a></p>
+</div>
+"""
